a713be88a151b47950c5fae8928a3f9bf120b044
KLGamet <therealklgamet@gmail.com>
2024-06-12 18:50:36 +0200

    test: add new leaderboard tests

--- src/app/features/leaderboard/leaderboard.component.spec.ts
+++ src/app/features/leaderboard/leaderboard.component.spec.ts
@@ -16,7 +16,21 @@ describe('LeaderboardComponent', () => {
         fixture.detectChanges()
     })
 
-    it('should create', () => {
-        expect(component).toBeTruthy()
+    it('should check if the Ligadaten have been loaded', () => {
+        component.ngOnInit();
+        const ligadaten: any = []
+        expect(component).toContain(ligadaten)
     })
+
+    it('should sort the Table by poinst(desc), goalDifference(desc), wins(desc) and name(asc)', () => {
+        component.ngOnInit();
+        expect(component).toBeDefined();
+    })
+
+    it('should have a lastDay variable that can be changed', () => {
+        component.ngOnInit();
+        var lastDay: Date
+        lastDay = component.changeLastDay(new Date())
+        expect(component).toContain(lastDay)
+    }) 
 })
